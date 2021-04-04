9977174167c827e4c87f4ef123bac9a048f49f61
Adrian Di Felice <adrian.di.felice@gmail.com>
2021-04-04 14:01:07 -0400

    get yelp data

--- src/components/yel.js
+++ src/components/yel.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styles from '../styles/discover.module.scss'
+
 
 function Discover () {
+  const [results, setResults] = useState([]);
+  const [term, setTerm ] = useState('');
   const url = 'http://localhost:3000/yelp'
-  const term = 'breakfast';
+  
+
+
 async function getYelpData () {
   const settings = {
   method:'post',
   headers: {
     "Content-type": "application/json"
   },
-  body: JSON.stringify({
+    body: JSON.stringify({
     name: term})
 }
 
@@ -18,19 +24,29 @@ async function getYelpData () {
     const message = 'There was an error' + response.status;
     throw new Error(message)
    }
-   const data = await response.text();
-   console.log(data)
-   
-   return data;
+   const data = await response.json();
+   console.log(data);
+   setResults(data) ;
 }
 
   return (
-    <>
-    <h1>Discover</h1>
-    <input></input>
-    <button onClick={()=>getYelpData()} > Click mee</button>
-</>
+    <div >
+        <div className={styles.wrapper}>
+          <input placeholder='enter keyword' onKeyUp={(e)=> setTerm(e.target.value)}></input>
+          <button onClick={()=> getYelpData()} > Click me</button>
+        </div>
+        {results.length > 0? results.map(result => 
+          <>
+            <div>{result.name}</div> 
+            <div>{result.categories.map(cat => <div>{cat.title}</div>)}</div>
+            <div>{result.display_phone}</div>
+            <div>{result.phone}</div>
+            <div>{result.price}</div>
+            <div>{result.location.address1}</div>
+            <a href={result.url}>click</a>
+          </>):'nothing yet'}
+    </div>
   )
 }
 
-export default Discover;
+export default Discover; 
