7f067f51990dae49bc60112eb3fa10a07b9533f6
Adrian Di Felice <adrian.di.felice@gmail.com>
2021-04-08 11:16:57 -0400

    MainDash refactor

--- src/components/mainDash.js
+++ src/components/mainDash.js
@@ -1,25 +1,16 @@
 import styles from '../styles/maindash.module.scss'
-import React, { useContext, useState, useEffect } from 'react';
-import { AppContext } from '../context/prospectContext'
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import Typography from '@material-ui/core/Typography';
-import Form from './formiktest';
+import React from 'react';
 import Accord from './accordionMainDash';
 
 export default function Dashboard () {
-  // const [prospects, _,  __, deleteProspect, updateStatus] = useContext(AppContext);
-
- 
   
   return (
-  <>
+  <div className={styles.wrapper}>
     <Accord title={'Not Contacted'}/>
     <Accord title={'Contacted'}/>
     <Accord title={'Responded'}/>
     <Accord title={'Meeting'}/>
     <Accord title={'Closed'}/>
-  </>
+  </div>
   )
 }
